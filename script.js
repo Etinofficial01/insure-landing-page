893a0fc8ce4bcd3db550e9d88e6b258f63fa29c0
Etinofficial01 <emmanuelosemwengie@gmail.com>
2025-06-23 11:40:33 +0100

    Insure landing page built with HTML, Tailwind CSS and JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const navBar = document.getElementById("navbar");
 const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
 
 menuBtn.addEventListener("click", () => {
     navBar.classList.toggle("open");
-})
+
+    menuBtn.classList.toggle("hidden");
+    closeBtn.classList.toggle("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+    navBar.classList.remove("open");
+
+    menuBtn.classList.remove("hidden");
+    closeBtn.classList.add("hidden");
+});
+
+window.onscroll = () => {
+    menuBtn.classList.remove("hidden");
+    navBar.classList.remove("open");
+    closeBtn.classList.add("hidden");
+}
